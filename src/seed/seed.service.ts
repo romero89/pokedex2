174f3d1e226bbad46ee196484fef181d5af6e3b1
Edgardo Romero <edgardoromeromolina89@gmail.com>
2025-09-09 16:42:57 -0600

    Seccion 8 terminada

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,28 +1,51 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 
 @Injectable()
 export class SeedService {
 
-  private readonly axios: AxiosInstance = axios;
+  
+
+  constructor(
+      @InjectModel( Pokemon.name )
+      private readonly pokemonModel: Model<Pokemon>,
+
+      private readonly http: AxiosAdapter,
+    ) {}
   
   async executeSeed() {
 
+    await this.pokemonModel.deleteMany({});
+
     
     // esta es la mejor manera serializamos la resp y solo mandamos a pedir lo que nos interesa como ser la data en este caso
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=1');
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
 
-    data.results.forEach(({name, url}) => {
+    // const insertPromisesArray = []; 
+
+    const pokemonToInsert: {name: string, no: number}[] = [];
+
+    data.results.forEach(async({name, url}) => {
 
       const segments = url.split('/');
       const no: number = +segments[ segments.length - 2 ]
+
+      // const pokemon = await this.pokemonModel.create({name, no});
+       pokemonToInsert.push({name, no});
+
+      // insertPromisesArray.push(
+      //   this.pokemonModel.create({name, no})
+      // );
       
-      console.log({name, no});
       
-      
-    })
+    });
+
+    await this.pokemonModel.insertMany(pokemonToInsert);
 
 
     // Esto no se debe hacer
@@ -33,7 +56,9 @@ export class SeedService {
     // return resp.data.results;
 
 
-    return data.results;
+    // return data.results;
+
+    return 'Seed Executed';
     
   }
 
